9872ece9f8c92f38b0d1401077c86527d54af401
Lanrewaju-adesh <larryadesh003@gmail.com>
2025-01-29 11:58:14 -0500

    Import Utils install numpy

---
+++ main.py
@@ -0,0 +1,9 @@
+from utils import *
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    get_array_sum()
---
+++ utils.py
@@ -0,0 +1,8 @@
+import numpy as np
+def  get_array_sum():
+#Create a 2D array
+    arr = np.array([[1, 2, 3], [4, 5, 6]])
+    print(arr)
+    print(arr[1, 2])
+# Perform a mathematical operation
+    return arr.sum()
